cde8c4a9f6ebcaaf709c348e89eafba32932ecdd
Felipe Ukan Pereira <felipe.ukan@gmail.com>
2025-11-10 23:59:26 -0500

    initial commit

--- gcp_projects/empty.tf
+++
@@ -1,5 +0,0 @@
-module "${PROJECT_NAME}" {
-  source       = "git::https://github.com/your_org/terraform-gcp-project.git?ref=v1.0.0"
-  project_name = "${PROJECT_NAME}"
-  project_type = "${PROJECT_TYPE}"
-}
